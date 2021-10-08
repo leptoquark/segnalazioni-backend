4186ea97bc057e4317535bd120194f680492f860
Claudio Biancalana <c.biancalana@anticorruzione.it>
2021-10-08 12:41:00 +0200

    primo commit

--- src/main/java/it/anac/segnalazioni/backend/SegnalazioniBackendApplication.java
+++ src/main/java/it/anac/segnalazioni/backend/SegnalazioniBackendApplication.java
@@ -2,14 +2,14 @@ package it.anac.segnalazioni.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SegnalazioniBackendApplication
 {
-	  @RequestMapping("/")
+	  @GetMapping("/")
 	  public String home() {
 	    return "SEGNALAZIONI - BACKEND API <a href=\"/swagger-ui/\">SWAGGER</a>";
 	  }
