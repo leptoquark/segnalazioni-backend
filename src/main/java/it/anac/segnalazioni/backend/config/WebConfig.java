4580571b8cb7b157d6573cd66eae69f901f134af
Claudio Biancalana <c.biancalana@anticorruzione.it>
2021-11-17 14:01:13 +0100

    aggiornamento

--- src/main/java/it/anac/segnalazioni/backend/config/WebConfig.java
+++ src/main/java/it/anac/segnalazioni/backend/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
