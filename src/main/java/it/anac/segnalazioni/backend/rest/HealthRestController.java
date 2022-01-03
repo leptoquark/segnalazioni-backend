03dd1c3dac52f37e2223ff1638e3973dae7d74d1
Claudio Biancalana <c.biancalana@anticorruzione.it>
2022-01-03 11:02:08 +0100

    aggiornamento

--- src/main/java/it/anac/segnalazioni/backend/rest/HealthRestController.java
+++ src/main/java/it/anac/segnalazioni/backend/rest/HealthRestController.java
@@ -22,40 +22,29 @@ public class HealthRestController {
 	@Autowired
 	private AppaltiServiceAdapter appaltiService;
 		
-	public Appalto getAppaltoFromCIG(String cig)
+	public Appalto getAppaltoFromCIG(String cig) throws JsonMappingException, JsonProcessingException
 	{
 		// XA31927D46
 		ObjectMapper om = new ObjectMapper();
 		Appalto appalto = null;
-		try {
-			appalto = om.readValue(appaltiService.getAppaltoFromCIG(cig), Appalto.class);
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
+		appalto = om.readValue(appaltiService.getAppaltoFromCIG(cig), Appalto.class);
+
 		return appalto;
 	}
 	
 	@Autowired
 	private PersonaGiuridicaServiceAdapter personaGiuridicaService;
 	
-	public PersonaGiuridica[] getPGFromDenominazione(String denominazioneLike, int page, int size) 
+	public PersonaGiuridica[] getPGFromDenominazione(String denominazioneLike, int page, int size) throws JsonMappingException, JsonProcessingException 
 	{
 		ObjectMapper om = new ObjectMapper();
 		PersonaGiuridica[] pg = null;
-		
-		try {
+
 			pg = om.readValue(personaGiuridicaService.
 					getPersonaGiuridicaFromDenominazioneLike(
 							denominazioneLike,
 							page,
 							size), PersonaGiuridica[].class);
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
 		
 		return pg;
 	}
@@ -64,19 +53,37 @@ public class HealthRestController {
    @GetMapping("/health")
    ResponseHealth test() {
 	   
-	   ResponseHealth status = new ResponseHealth();
-	   status.setStatusOK();
+	    ResponseHealth status = new ResponseHealth();
+	    status.setStatusOK();
 	   
-	   if (!getAppaltoFromCIG("XA31927D46").codice_risposta.toString().equals("OK")) {
-		   status.setMessage("Il Servizio CIG non risponde correttamente!");
-		   status.setStatusKO();
-	   } else
-	   if(getPGFromDenominazione("laziocrea", 0, 10)[0].id.toString().equals("")) {
-		   status.setMessage("Il Servizio Persona Giuridica non risponde correttamente!");
-		   status.setStatusKO();
-	   }
+	    String res_appalti;
+		try {
+			res_appalti = getAppaltoFromCIG("XA31927D46").codice_risposta.toString();
+		} catch (JsonMappingException e) {
+			res_appalti = "KO";
+		} catch (JsonProcessingException e) {
+			res_appalti = "KO";
+		}
+		   
+		String res_pg;
+		try {
+			res_pg = getPGFromDenominazione("laziocrea", 0, 10)[0].id.toString();
+		} catch (JsonMappingException e) {
+			res_pg = "";
+			
+		} catch (JsonProcessingException e) {
+			res_pg = "";
+		}
+
+		if (!res_appalti.equals("OK")) {
+			   status.setMessage("Il Servizio CIG non risponde correttamente!");
+			   status.setStatusKO();
+		   } else
+		   if(res_pg.equals("")) {
+			   status.setMessage("Il Servizio Persona Giuridica non risponde correttamente!");
+			   status.setStatusKO();
+		   }
 	   
 	   return status;
    }
-
 }
